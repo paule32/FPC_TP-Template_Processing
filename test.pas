380df9af4ef517f43b3da3883570ae910556daf9
paule32 <paule32.jk@gmail.com>
2025-03-09 23:11:50 +0100

    update

---
+++ test.pas
@@ -0,0 +1,49 @@
+var
+  TOCtext: String;
+  
+begin
+  TOCtext :=
+'Pascal Zeichen und Symbole' + #10+
+'    Symbole' + #10+
+'    Kommentare' + #10+
+'    Reservierte Schlüsselwörter' + #10+
+'        Turbo Pascal' + #10+
+'        Object Pascal' + #10+
+'        Modifikationen' + #10+
+'    Kennzeichnungen' + #10+
+'    Hinweise für Direktiven' + #10+
+'    Zahlen' + #10+
+'    Bezeichner' + #10+
+'    Zeichenketten' + #10+
+'Konstanten' + #10+
+'    Gewöhnliche' + #10+
+'    Typisierte' + #10+
+'    Resourcen Zeichenketten' + #10+
+'Typen' + #10+
+'    Basis-Typen' + #10+
+'        Ordinale Typen' + #10+
+'        Ganze Zahlen (Integer)' + #10+
+'        Boolesche Typen' + #10+
+'        Aufzählungen' + #10+
+'        Untermengen' + #10+
+'        Zeichen' + #10+
+'    Zeichen-Typen' + #10+
+'        Char oder AnsiChar' + #10+
+'        WideChar' + #10 +
+'        Sonstige' + #10+
+'        Einzel-Byte Zeichenketten' + #10+
+'            ShortString' + #10+
+'            AnsiString' + #10+
+'            Zeichen-Code Umwandlung' + #10+
+'            RawByteString' + #10+
+'            UTF8String' + #10+
+'        Multi-Byte Zeichenketten' + #10+
+'            UnicodeString' + #10+
+'            WideString' + #10+
+'        Konstante Zeichenketten' + #10+
+'        Nullterminierente Zeichenketten (PChar)' + #10+
+'        Zeichenketten-Größen' + #10     +
+'    Strukturierte Typen' + #10         +
+'        Gepackte Struktur-Typen' + #10+
+'        Array''s' + #10;  
+end.
